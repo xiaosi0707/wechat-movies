d57bc3a088deedfeb612bf40945bec3a1172a634
wyunfei <583514368@qq.com>
2018-02-25 18:21:12 +0800

    上滑加载

--- pages/movie/list/list.js
+++ pages/movie/list/list.js
@@ -1,6 +1,6 @@
 // pages/movie/list/list.js
+var start = 0;
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -12,14 +12,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
     // 动态设置title
     wx.setNavigationBarTitle({
       title: options.title
     })
     var _this = this;
     wx.request({
-      url: 'http://t.yushu.im/v2/movie/in_theaters?start=0&count=20',
+      url: 'http://t.yushu.im/v2/movie/in_theaters?start=0&count=10',
       header: {
         'content-type': 'application/text'
       },
@@ -33,53 +32,26 @@ Page({
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onScrollLower: function () {
+    var _this = this;
+    start+=10;
+    console.log(start);
+    wx.request({
+      url: 'http://t.yushu.im/v2/movie/in_theaters?start=' + start + '&count= 10',
+      header: {
+        'content-type': 'application/text'
+      },
+      success: function (res) {
+        for (var i = 0; i < res.data.subjects.length; i++) {
+          _this.data.movies.push(res.data.subjects[i])
+        }
+        _this.setData({
+          'movies': _this.data.movies
+        })
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
   }
 })
